d0097fb2f781230e17f5a64750d08ab0d73be831
Huggable Hacker <huggablehacker@users.noreply.github.com>
2016-07-23 21:50:49 -0400

    Create fdns.sh

---
+++ fdns.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	echo "[*] Forward DNS Lookup"
+	echo "[*] Usage	: $0 <Enter your target domain, ie google.com> "
+	exit 0
+fi
+
+for name in $(cat list.txt);do
+	host $name.$1|grep "has address" |cut -d" " -f1,4
+done
